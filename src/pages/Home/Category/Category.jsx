7db6a2d07dc4bed98af7e08dc8202158391ef04a
msifatsadiq <msifatsadiq@gmail.com>
2024-05-16 15:59:15 +0600

    adding more the category section

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -10,32 +10,36 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-
-// import './styles.css';
-
 // import required modules
 import { Pagination } from 'swiper/modules';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle'
 
 
 const Category = () => {
     return (
-        <Swiper
-            slidesPerView={4}
-            spaceBetween={30}
-            centeredSlides={true}
-            pagination={{
-                clickable: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper mb-20"
-        >
-            <SwiperSlide> <img src={slide1} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Salad</p> </SwiperSlide>
-            <SwiperSlide> <img src={slide2} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Pizzas</p> </SwiperSlide>
-            <SwiperSlide> <img src={slide3} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Soups</p> </SwiperSlide>
-            <SwiperSlide> <img src={slide4} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Dessert</p> </SwiperSlide>
-            <SwiperSlide> <img src={slide5} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Salad</p> </SwiperSlide>
+        <section>
+            <SectionTitle
+                subHeading={"From 11.00am to 10.00pm"}
+                heading={"Order Online"}
+            ></SectionTitle>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper mb-20"
+            >
+                <SwiperSlide> <img src={slide1} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Salad</p> </SwiperSlide>
+                <SwiperSlide> <img src={slide2} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Pizzas</p> </SwiperSlide>
+                <SwiperSlide> <img src={slide3} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Soups</p> </SwiperSlide>
+                <SwiperSlide> <img src={slide4} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Dessert</p> </SwiperSlide>
+                <SwiperSlide> <img src={slide5} alt="" /> <p className='text-4xl text-center uppercase text-white -mt-16 '>Salad</p> </SwiperSlide>
 
-        </Swiper>
+            </Swiper>
+        </section>
     );
 };
 
