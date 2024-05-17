ddad09740fa25bbfe5311506559e6b4710f79260
msifatsadiq <msifatsadiq@gmail.com>
2024-05-17 18:42:55 +0600

    adding the order section

---
+++ src/pages/Order/Order/Order.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import orderCover from '../../../assets/shop/banner2.jpg'
+import Cover from '../../Shared/Cover/Cover';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+const Order = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+
+
+    return (
+        <div>
+            <Cover img={orderCover} title="Order Food"></Cover>
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => console.log(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drinks</Tab>
+
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
+        </div>
+    );
+};
+
+export default Order;
