dfbf87912d5ace944e27c0121f92cead700a12fa
msifatsadiq <msifatsadiq@gmail.com>
2024-05-19 12:11:42 +0600

    adding firebase

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
+import AuthProvider from './providers/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className='w-10/12 mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className='w-10/12 mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
